81e4a9b2b3a8acdb2a939abbe7041dd8adbb99d3
Mick Zijdel <mickzijdel@hotmail.com>
2024-09-20 00:31:05 +0100

    Add live scoreboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @teams = User.where(role: :team).includes(:results).sort_by { |team| -team.results.sum(&:total_points) }
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @teams.map { |team| { id: team.id, username: team.username, score: team.results.sum(&:total_points) } } }
+    end
   end
 end
