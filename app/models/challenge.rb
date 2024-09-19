ea9841d2b2051e39f2ceb77170feb189bfd4ba51
Mick Zijdel <mickzijdel@hotmail.com>
2024-09-19 23:19:18 +0100

    Remove restriction of non-negative points on challenges

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,5 +1,5 @@
 class Challenge < ApplicationRecord
   validates :number, :description, :points, presence: true
-  validates :points, :number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :points, :number, numericality: { only_integer: true }
   validates :number, :description, uniqueness: true
 end
