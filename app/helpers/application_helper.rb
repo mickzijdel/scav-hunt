d80e8ecc8441dc4ca4796b1d0154d9a5675412a5
Mick Zijdel <mickzijdel@hotmail.com>
2024-09-22 12:17:49 +0100

    Add navbar in the same way as Black Lightning

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def navbar_items
+    navbar_items = [
+      { title: "Scoreboard",            path: root_path },
+      { title: "Challenges",            path: challenges_path },
+      { title: "Users",                 path: users_path }
+    ]
+
+    # Display the login link if the user is not signed in yet, otherwise display a link to the admin site and a link to log out.
+    if user_signed_in?
+      navbar_items << { title: "Log Out", path: destroy_user_session_path, method: :delete, item_class: "border border-white rounded-3" }
+    else
+      navbar_items << { title: "Log In", path: new_user_session_path, item_class: "border border-white rounded-3" }
+    end
+
+    navbar_items
+  end
 end
