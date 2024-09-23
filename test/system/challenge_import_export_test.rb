58bd663f7f0e362b40d8bc44420e3fdb566eb2b0
Mick Zijdel <mickzijdel@hotmail.com>
2024-09-23 19:20:48 +0100

    Test overwriting existing challenges with csv

--- test/system/challenge_import_export_test.rb
+++ test/system/challenge_import_export_test.rb
@@ -26,8 +26,29 @@ class ChallengeImportExportTest < ApplicationSystemTestCase
       assert_selector "td", text: "New Challenge"
     end
   end
+  test "overwriting existing challenges" do
+    existing_challenge = challenges(:one)
+    old_description = existing_challenge.description
 
-  # TODO: Overwriting challenges
+    # Prepare a test CSV file with updated data
+    csv_content = "Number,Description,Points\n#{existing_challenge.number},Updated Description,300"
+    file = Tempfile.new([ "test_import", ".csv" ])
+    file.write(csv_content)
+    file.rewind
+
+    visit challenges_path
+    click_on "Import Challenges"
+
+    attach_file("file", file.path)
+    click_on "Import"
+
+    assert_text "Challenges imported successfully"
+
+    existing_challenge.reload
+    assert_not_equal old_description, existing_challenge.description
+    assert_equal "Updated Description", existing_challenge.description
+    assert_equal 300, existing_challenge.points
+  end
 
   test "exporting challenges" do
     visit challenges_path
