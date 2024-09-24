03c4a5509702175cf5ca4d531f33c862d1e07016
Mick Zijdel <mickzijdel@hotmail.com>
2024-09-24 09:32:47 +0100

    Remove the allow_browser versions: :modern check as it breaks on several not that outdated browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
   rescue_from CanCan::AccessDenied do |exception|
     # FIXME: Render proper access denied page. See Black Lightning
     redirect_to root_path, alert: "I'm sorry, I can't let you do that"
